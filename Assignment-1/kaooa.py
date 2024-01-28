578a862cf57c6d594c593c934a7f19f0bd0beacd
PraneethJain <praneethjain005@gmail.com>
2024-01-28 12:03:05 +0530

    kaooa adjacent movement

--- Assignment-1/kaooa.py
+++ Assignment-1/kaooa.py
@@ -14,45 +14,27 @@ class Turn(Enum):
     Vulture = auto()
 
 
-class State:
-    def __init__(self) -> None:
-        self.turn = Turn.Crow
-        self.num_crows = 0
-        self.num_vultures = 0
-        self.MAX_CROWS = 7
-
-        self.selected_crow = None
-        self.vulture_point = None
-
-    def next_turn(self) -> None:
-        match self.turn:
-            case Turn.Crow:
-                if self.num_crows < self.MAX_CROWS:
-                    self.num_crows += 1
-                self.turn = Turn.Vulture
-                if self.vulture_point is not None:
-                    self.vulture_point.scale = 0.5
-            case Turn.Vulture:
-                self.vulture_point.scale = 0.3
-                if self.num_vultures == 0:
-                    self.num_vultures += 1
-                self.turn = Turn.Crow
-
-
 class PointState(Enum):
     Empty = auto()
     Crow = auto()
     Vulture = auto()
 
 
+class PointType(Enum):
+    Inner = auto()
+    Outer = auto()
+
+
 class Point(Sprite):
     instances = []
 
-    def __init__(self, x, y) -> None:
+    def __init__(self, x: float, y: float, point_type: PointType, index: int) -> None:
         super().__init__(texture=None, model="circle", collider="sphere")
         self.to_empty()
         self.x = x
         self.y = y
+        self.point_type = point_type
+        self.index = index
         Point.instances.append(self)
 
     def to_empty(self) -> None:
@@ -88,9 +70,10 @@ class Point(Sprite):
                             pass
                         case PointState.Empty:
                             # Try to move vulture here
-                            state.vulture_point.to_empty()
-                            self.to_vulture()
-                            state.next_turn()
+                            if state.move_possible_adjacent(self):
+                                state.vulture_point.to_empty()
+                                self.to_vulture()
+                                state.next_turn()
             case Turn.Crow:
                 if state.num_crows == state.MAX_CROWS:
                     match self.point_state:
@@ -98,14 +81,10 @@ class Point(Sprite):
                             # Ignore
                             pass
                         case PointState.Crow:
-                            # Select this point
-                            if state.selected_crow is not None:
-                                state.selected_crow.scale = 0.3
-                            state.selected_crow = self
-                            state.selected_crow.scale = 0.5
+                            state.select_crow(self)
                         case PointState.Empty:
                             # Try to move selected point here
-                            if state.selected_crow is not None:
+                            if state.move_possible_adjacent(self):
                                 state.selected_crow.to_empty()
                                 state.selected_crow = None
                                 self.to_crow()
@@ -116,12 +95,78 @@ class Point(Sprite):
                         state.next_turn()
 
 
+class State:
+    def __init__(self) -> None:
+        self.turn = Turn.Crow
+        self.num_crows = 0
+        self.num_vultures = 0
+        self.MAX_CROWS = 3
+
+        self.selected_crow: None | Point = None
+        self.vulture_point: None | Point = None
+
+    def next_turn(self) -> None:
+        match self.turn:
+            case Turn.Crow:
+                if self.num_crows < self.MAX_CROWS:
+                    self.num_crows += 1
+                self.turn = Turn.Vulture
+                if self.vulture_point is not None:
+                    self.vulture_point.scale = 0.5
+            case Turn.Vulture:
+                self.vulture_point.scale = 0.3
+                if self.num_vultures == 0:
+                    self.num_vultures += 1
+                self.turn = Turn.Crow
+
+    def select_crow(self, crow: Point) -> None:
+        if self.selected_crow is not None:
+            self.selected_crow.scale = 0.3
+        self.selected_crow = crow
+        self.selected_crow.scale = 0.5
+
+    def move_possible_adjacent(self, end: Point) -> bool:
+        if (self.turn == Turn.Crow and self.selected_crow is None) or (
+            self.turn == Turn.Vulture and self.vulture_point is None
+        ):
+            return False
+
+        start = self.selected_crow if self.turn == Turn.Crow else self.vulture_point
+        match start.point_type:
+            case PointType.Inner:
+                match end.point_type:
+                    case PointType.Inner:
+                        return end.index in (
+                            (start.index - 1) % 5,
+                            (start.index + 1) % 5,
+                        )
+                    case PointType.Outer:
+                        return end.index in (start.index, (start.index - 1) % 5)
+            case PointType.Outer:
+                match end.point_type:
+                    case PointType.Inner:
+                        return end.index in (
+                            start.index,
+                            (start.index + 1) % 5,
+                        )
+                    case PointType.Outer:
+                        return False
+
+
 if __name__ == "__main__":
     state = State()
     d = sin(126 * pi / 180) / sin(18 * pi / 180)
-    inner_points = [Point(sin(2 * pi * i / 5), cos(2 * pi * i / 5)) for i in range(5)]
+    inner_points = [
+        Point(sin(2 * pi * i / 5), cos(2 * pi * i / 5), PointType.Inner, i)
+        for i in range(5)
+    ]
     outer_points = [
-        Point(d * sin(2 * pi * i / 5 + pi / 5), d * cos(2 * pi * i / 5 + pi / 5))
+        Point(
+            d * sin(2 * pi * i / 5 + pi / 5),
+            d * cos(2 * pi * i / 5 + pi / 5),
+            PointType.Outer,
+            i,
+        )
         for i in range(5)
     ]
     lines = [
